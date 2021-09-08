7821ba32b37eeff0d35abde00dd4d73423271195
Cbukuba <chrisbukuba058@gmail.com>
2021-09-08 21:47:29 +0200

    Ex0405

--- Cbukuba/C00/ex05/ft_print_comb.c
+++ Cbukuba/C00/ex05/ft_print_comb.c
@@ -1,36 +1,58 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cbukuba <chrisbukuba058@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/08 13:04:47 by cbukuba           #+#    #+#             */
-/*   Updated: 2021/09/08 14:13:31 by cbukuba          ###   ########.fr       */
+/*   Created: 2021/09/08 14:25:19 by cbukuba           #+#    #+#             */
+/*   Updated: 2021/09/08 21:39:08 by cbukuba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_is_negative(int n)
+void	write_chfr(int a, int b, int c)
 {
-	if (n < 0)
-	{
-		write(1, "N", 1);
-	}
-	else
-	{
-		write(1, "P", 1);
-	}
-	return (0);
+	char	tab[4];
+	tab[0] = a + '0';
+	tab[1] = b + '0';
+	tab[2] = c + '0';
+	tab[4] = ',';
+
+	write(1, &tab, 4);
 }
 
-/*int	main(void)
+void	ft_print_comb(void)
 {
-	int	n;
+	int	a;
+	int	b;
+	int	c;
 
-	n = -6;
-	ft_is_negative(-6);
+	a = 0;
+	b = 1;
+	c = 2;
+	
+	while(a <= 7)
+	{
+		while(b <= 8)
+		{
+			while(c <= 9)
+			{
+				write_chfr(a , b, c);
+				c++;
+			}
+			b++;
+			c = b + 1;
+		}
+		a++;
+		b = a + 1;
+		c = b + 1;
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb();
 	return (0);
 }
-*/
