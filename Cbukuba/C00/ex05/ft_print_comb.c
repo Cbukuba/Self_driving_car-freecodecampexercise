23824f39d4fe84bd7b33410d82946999f2d0329a
Cbukuba <chrisbukuba058@gmail.com>
2021-09-08 22:03:52 +0200

    >05

--- Cbukuba/C00/ex05/ft_print_comb.c
+++ Cbukuba/C00/ex05/ft_print_comb.c
@@ -6,7 +6,7 @@
 /*   By: cbukuba <chrisbukuba058@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 14:25:19 by cbukuba           #+#    #+#             */
-/*   Updated: 2021/09/08 21:39:08 by cbukuba          ###   ########.fr       */
+/*   Updated: 2021/09/08 22:03:06 by cbukuba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 void	write_chfr(int a, int b, int c)
 {
 	char	tab[4];
+
 	tab[0] = a + '0';
 	tab[1] = b + '0';
 	tab[2] = c + '0';
 	tab[4] = ',';
-
 	write(1, &tab, 4);
 }
 
@@ -32,14 +32,13 @@ void	ft_print_comb(void)
 	a = 0;
 	b = 1;
 	c = 2;
-	
-	while(a <= 7)
+	while (a <= 7)
 	{
-		while(b <= 8)
+		while (b <= 8)
 		{
-			while(c <= 9)
+			while (c <= 9)
 			{
-				write_chfr(a , b, c);
+				write_chfr(a, b, c);
 				c++;
 			}
 			b++;
